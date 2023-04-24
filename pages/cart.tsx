1cadd7e617a956beb67d7a44ba59f93b2e5cf352
dimeji-code <dimeji.situ@yahoo.com>
2023-04-24 18:55:36 -0400

    cart update

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -8,7 +8,7 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const Cart = () => {
-  const selectCart = useSelector((state:any) => state.cart)
+  const selectCart = useSelector((state:RootState) => state.cart)
   // const itemAmount = useSelector((state:any) => state.cart.totalAmount)
   const [itemArray, setItemArray] = useState<any>([])
   const [localAmount, setLocalAmount] = useState<number>()
@@ -60,25 +60,25 @@ const Cart = () => {
   return (
     <div className='px-5 md:px-[10%] pt-[10vh] flex flex-col h-full items-center'>
       <ToastContainer  />
-        <h4 className='font-semibold text-2xl py-4'>Cart Items</h4>
+        <h4 className='font-semibold text-xl md:text-2xl py-2 md:py-4'>Cart Items</h4>
         <h4 className='text-sm md:text-base subpixel-antialiased font-light md:font-normal mb-2 '>Total Items: {localNumber}</h4>
-        <h4 className='text-sm md:text-base subpixel-antialiased font-light md:font-normal mb-2 '>Total Amount: ${(localAmount?.toFixed(2)) }</h4>
+        <h4 className='hidden md:flex text-sm md:text-base subpixel-antialiased font-light md:font-normal mb-2 '>Total Amount: ${(localAmount?.toFixed(2)) }</h4>
 
     {itemArray.length > 0  && 
     <>
-      <div>
         <button 
-          className='font-light text-md border-[1.1px] mr-2 border-black hover:border-gray-500 duration-700 hover:bg-gray-500 hover:text-white rounded-sm p-2' 
+          className='font-light text-md border-[1.1px]  border-black hover:border-gray-500 duration-700 hover:bg-gray-500 hover:text-white rounded-sm p-1 md:p-2' 
           onClick={clear}>
             clear cart
         </button>
         
-      </div>
         
         
-      <div className='flex flex-row w-full '>
-        <div className='w-[70%] md:w-4/5 flex flex-col mt-2 h-[70vh] overflow-y-scroll'>{ itemArray}</div>
-        <div className='w-[30%] md:w-1/5 m-2 '> 
+      <div className='flex flex-col md:flex-row w-full '>
+        {/* <div className='w-[70%] md:w-4/5 flex flex-col mt-2 h-[70vh] overflow-y-scroll'>{ itemArray}</div> */}
+        <div className='w-full md:w-4/5 flex flex-col mt-2 h-[40vh] md:h-[70vh] overflow-y-scroll'>{ itemArray}</div>
+        {/* <div className='w-[30%] md:w-1/5 m-2 '>  */}
+        <div className=' w-1/2  md:w-1/5 m-2 '> 
             <div className='border shadow-sm bg-slate-50 w-full p-2 m-2'>
               <h3 className='text-base subpixel-antialiased font-normal mb-2 '>Checkout</h3>
               <h3 className='text-sm font-light mb-2 '>cost: ${localAmount?.toFixed(2)}</h3>
@@ -86,7 +86,7 @@ const Cart = () => {
               <hr className='border-b-black'/>
               <h3 className='text-sm font-normal my-2 '>total: ${calculateAmt(Number(localAmount))}</h3>
               <button 
-                className='font-light text-base border-[1.1px] border-black hover:border-[#519e59] duration-700 hover:bg-[#519e59] hover:text-white rounded-sm p-2' 
+                className='font-light text-base border-[1.1px] border-black hover:border-[#519e59] duration-700 hover:bg-[#519e59] hover:text-white rounded-sm p-1 md:p-2' 
                 onClick={()=> {notify();clear();}}>
                   pay now
               </button>
